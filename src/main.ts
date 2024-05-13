a5d2ca98aed3fc85a8eaf398c66de279001a3208
Hashan Liyanage <hashanduplessis@gmail.com>
2024-05-13 23:37:05 +0530

    feat: pinia added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia' // import the pinia from library
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(createPinia) // access the pinia in global scope
 
 app.mount('#app')
